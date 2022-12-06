69c0e3dbec674c2fa7b09590adaf08949f694938
Tobias Olausson <tobias.olausson@yh.nackademin.se>
2022-12-06 11:18:52 +0100

    bort med materialize, header footer

--- index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var sel = document.querySelectorAll('select');
-    M.FormSelect.init(sel);
-  });
-
-const slideMenu = document.querySelectorAll(".sidenav");
-M.Sidenav.init(slideMenu, {});
